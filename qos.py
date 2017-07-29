814c637ae7177328dd61020f368ef7949e626c26
Zhou Yaoyang <diamondzyy@sina.com>
2017-07-29 13:37:45 +0800

    Tuning figures

--- qos.py
+++ qos.py
@@ -20,6 +20,8 @@ def get_qos_dict(filename):
     for index, row in df_raw.iterrows():
         d[index] = row.loc['QoS_0']
     df = pd.DataFrame.from_dict(d, orient='index')
+    df.loc['mean'] = df.mean(axis=0)
+    df.loc['std'] = df.std(axis=0)
     return df
 
 
@@ -29,26 +31,29 @@ def draw(df: pd.DataFrame, width: float,
     x_labels = tuple(df.index)
     ind = np.arange(len(x_labels))
 
-    print(df)
+    # print(df)
     # sys.exit(0)
     rects = ax.bar(ind + offset, df[0], width, color=color)
     ax.set_ylabel('Normalized IPC')
     ax.set_title('Normalized IPC by pairs')
-    ax.set_xticks(ind + width/2)
+    ax.set_xticks(ind + width*2.5)
     if (set_xtickname):
         xtick_names = ax.set_xticklabels(x_labels)
-        plt.setp(xtick_names , rotation=90, fontsize=12)
+        plt.setp(xtick_names , rotation=90, fontsize=7)
 
     return rects
 
 
 fig, ax = plt.subplots()
 ax.set_ylim([0, 1.2])
-fig.set_size_inches((14, 6))
+ax.set_xlim([-0.5, 50])
+fig.set_size_inches((9, 3.5))
 
 df_dict = {
     'controlled_core_controlled_cache':
-        get_qos_dict('qos_48\\big_core_cc_cache_cc_core_3.csv'),
+        get_qos_dict('qos_48\\big_core_cc_cache_cc_core_6.csv'),
+    'controlled_fetch_controlled_cache':
+        get_qos_dict('qos_48\\big_core_cc_cache_fc_core_4.csv'),
     'part_core_comp_cache':
         get_qos_dict('qos_48\\big_core_comp_cache_part_core_48.csv'),
     'comp_core_comp_cache':
@@ -58,8 +63,8 @@ df_dict = {
 }
 
 iter_num = 0
-width = 0.2
-colors = sns.light_palette("grey", n_colors=5, reverse=True).as_hex()
+width = 0.18
+colors = sns.light_palette("grey", n_colors=6, reverse=True).as_hex()
 rects_list = []
 
 for k in df_dict:
@@ -71,7 +76,7 @@ for k in df_dict:
     iter_num += 1
 
 ax.legend([x[0] for x in rects_list],
-          [k for k in df_dict], fontsize='small')
+          [k for k in df_dict], fontsize='xx-small', ncol=5)
 
 ax.plot([-width, 48 - width], [0.9, 0.9], "k--")
 plt.tight_layout()
